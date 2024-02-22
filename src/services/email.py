13ccdc250f051ad24c45ff27149e528f923d6a56
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-02-22 17:16:20 +0100

    Added .env credentials for email

--- src/services/email.py
+++ src/services/email.py
@@ -3,21 +3,25 @@ from pydantic import EmailStr
 from pathlib import Path
 from src.services.auth import auth_service
 from fastapi_mail.errors import ConnectionErrors
+from src.load_dotenv import dotenv_load
+import os
 
+dotenv_load()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="example@meta.ua",
-    MAIL_PASSWORD="secretPassword",
-    MAIL_FROM="example@meta.ua",
-    MAIL_PORT=465,
-    MAIL_SERVER="smtp.meta.ua",
-    MAIL_FROM_NAME="Desired Name",
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
+
 
 async def send_email(email: EmailStr, username: str, host: str):
     try:
